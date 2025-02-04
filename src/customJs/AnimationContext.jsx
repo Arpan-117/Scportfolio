557c74cd7294aad7ace2fbbe4a6cbb0ec152ab99
Shubham Paul <iamshubhham@hotmail.com>
2025-02-04 16:32:22 +0530

    Completed the blog section with all of the responsive properties too

--- src/customJs/AnimationContext.jsx
+++ src/customJs/AnimationContext.jsx
@@ -13,3 +13,13 @@ export const item = {
   hidden: {y:'100%'},
   show: {y:"0%", transition:{duration: 0.5}}
 };
+export const item1 = {
+  hidden: {x:'100%'},
+  show: {x:"0%", transition:{duration: 0.5}},
+  exit: { 
+    opacity: 0, 
+    x: '-100%',
+    transition: { duration: 0.5 } 
+  }
+};
+
