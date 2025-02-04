12cccc05f30d0a9a6c97d0277da63ca2c1138df1
Shubham Paul <iamshubhham@hotmail.com>
2025-02-05 02:23:37 +0530

    Fine tuned the animations,& very importantly the navigation b/w pages

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,17 +4,27 @@ import About from '../components/About'
 import Services from '../components/Services'
 import Contact from '../components/Contact'
 import { useEffect } from 'react'
+import { useLocation } from 'react-router-dom'
 
 function Home() {
-  useEffect(()=>{
-    console.log('Home is getting rendered');
-  })
+  const location = useLocation()
+
+  useEffect(() => {
+    if (location.hash) {
+      const elementId = location.hash.substring(1)
+      const element = document.getElementById(elementId)
+      if (element) {
+        element.scrollIntoView({ behavior: 'smooth' })
+      } // adjust as needed based on your content loading
+    }
+  }, [location])
+
   return (
     <>
-        <Hero />
-        <About />
-        <Services />
-        <Contact />
+      <Hero />
+      <About />
+      <Services />
+      <Contact />
     </>
   )
 }
